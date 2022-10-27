0885defd96a39dd4411f48c8392fbbd9095c5fe8
MidKnightXI <romain.bouguet@epitech.eu>
2022-10-27 20:17:57 +0900

    feat: adding basic frontend UI

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,14 @@
     windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn patch() -> i32 {
+    return 1;
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![patch])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,33 @@
-import { createSignal } from "solid-js";
-import logo from "./assets/logo.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+import { createSignal } from "solid-js";
+
 import "./App.css";
+import opgg from "./assets/opgg.svg";
+
 
 function App() {
-  const [greetMsg, setGreetMsg] = createSignal("");
-  const [name, setName] = createSignal("");
+  const [status, setStatus] = createSignal("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name: name() }));
+  async function patch() {
+    const res = await invoke("patch") === 1 ?
+    "Patched Successfully"
+    :
+    "An Error Occured During The Patch";
+
+    setStatus(res);
   }
 
   return (
     <div class="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div class="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={logo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
-
-      <div class="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
-        </div>
-      </div>
-
-      <p>{greetMsg}</p>
+      <a href="https://github.com/MidKnightXI/opgg-ads-remover" target="_blank">
+        <img src={opgg} class="logo opgg" alt="OPGG logo" />
+      </a>
+      <p>Click on the logo to get redirected to the github page.</p>
+      <br/>
+      <button type="button" onClick={() => patch()}>
+        Patch
+      </button>
+      <p>{status}</p>
     </div>
   );
 }
